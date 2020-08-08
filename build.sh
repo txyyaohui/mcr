e2df9e47eedef77b746d5998155226aa8e1e4378
zhaofeng-shu33 <616545598@qq.com>
2020-08-08 22:01:33 +0800

    proper position

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 bash /etc/profile.d/modules.sh
-set -e -x
 module load slurm
 module load anaconda3 cuda90/toolkit/9.0.176 cudnn/7.0
+set -e -x
 # one cpu and one gpu
 srun --gres=gpu:1 -w node01 python3 k_mnist_xregression_simple.py 
 
