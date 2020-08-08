d9cc95481ae8c065e1482608898c1ca7585c04cf
zhaofeng-shu33 <616545598@qq.com>
2020-08-08 21:50:47 +0800

    add ci

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+bash /etc/profile.d/modules.sh
+module load slurm
+module load anaconda3 cuda90/toolkit/9.0.176 cudnn/7.0
+# one cpu and one gpu
+srun --gres=gpu:1 -w node01 python3 k_mnist_xregression_simple.py 
+
